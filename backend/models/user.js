1203d44b2398f44ef1f6109725241f70cc6a222d
UditTarini <udit.tarini937@gmail.com>
2020-07-04 21:57:11 +0530

    user schema

---
+++ backend/models/user.js
@@ -0,0 +1,72 @@
+var mongoose = require('mongoose')
+const crypto = require ('crypto')
+const uuidv1 = require('uuid/v1')
+var Schema = mongoose.Schema
+
+var user_schema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        maxlength: 40,
+        trim: true
+    },
+    lastname:{
+        type: String,
+        maxlength: 40,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        maxlength: 40,
+        trim: true,
+        unique:true
+    },
+    userinfo:{
+        type:String,
+        trim:true
+    },
+    encrypted_password:{
+        type: String,
+        required: true
+    },
+    salt:String,
+    role:{
+        type:Number,
+        default:0
+    },
+    purchase: {
+        type: Array,
+        default:[]
+    }
+})
+
+user_schema.virtual("password")
+    .set(function(password){
+        this._password = password
+        this.salt = uuidv1()
+        this.encrypted_password = this.getSecurePassword(password)
+    })
+    .get(function(){
+        return this._password
+    })
+
+user_schema.method = {
+
+    getSecurePassword: function(userpassword){
+        if(!password) return ""
+        try {
+            return crypto.createHmac('sha256', this,salt)
+            .update(userpassword)
+            .digest('hex')
+        } catch (error) {
+            return ""
+        }
+    },
+    authenticate: function(userpassword){
+        return this.getSecurePassword(userpassword) === this.encrypted_password
+    }
+}
+
+module.exports = mongoose.model("user",user_schema)
+
