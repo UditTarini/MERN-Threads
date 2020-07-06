65c3c6f7cb424850873ad89537c8357fcef12d4a
UditTarini <udit.tarini937@gmail.com>
2020-07-06 20:08:34 +0530

    athentication process

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,20 +1,22 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-var {ObjectId} = mongoose.Schema
+var {
+    ObjectId
+} = mongoose.Schema
 
 
-const orderSchema = new Schema(
-    {
-        products:[productCartSchema],
-        transaction_id:{},
-        amount:Number,
-        address: String,
-        updated: Date,
-        user:{
-            type:ObjectId,
-            ref:"user"
-        }
-    }, {timestamps: true}
-)
+const orderSchema = new Schema({
+    products: [productCartSchema],
+    transaction_id: {},
+    amount: Number,
+    address: String,
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "user"
+    }
+}, {
+    timestamps: true
+})
 
-module.exports = mongoose.model("order", orderSchema)
+module.exports = mongoose.model("Order", orderSchema)
