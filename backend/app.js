574e4d3da467d944d2bb885c57a177e28fe55dc7
UditTarini <udit.tarini937@gmail.com>
2020-07-06 12:28:12 +0530

    routers and middlewares

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ require('dotenv').config()
 const mongoose = require("mongoose")
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
-mongoose.connect(process.env.DB_URL,{
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>console.log("DB CONNECTED"))
+const auth = require("./routes/auth")
 
+// connection
+mongoose.connect(process.env.DB_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("DB CONNECTED"))
+
+
+// middleware
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+// route
+app.use("/api", auth)
+
+// port
 const port = process.env.PORT || 8000
-
-app.listen(port, ()=>console.log(`server running at port ${port}`))
+app.listen(port, () => console.log(`server running at port ${port}`))
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,72 +1,79 @@
 var mongoose = require('mongoose')
-const crypto = require ('crypto')
-const uuidv1 = require('uuid/v1')
+const crypto = require('crypto')
+const {
+    v1: uuidv1
+} = require('uuid');
+
 var Schema = mongoose.Schema
 
 var user_schema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true,
         maxlength: 40,
         trim: true
     },
-    lastname:{
+    lastname: {
         type: String,
         maxlength: 40,
         trim: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         maxlength: 40,
         trim: true,
-        unique:true
+        unique: true
     },
-    userinfo:{
-        type:String,
-        trim:true
+    userinfo: {
+        type: String,
+        trim: true
     },
-    encrypted_password:{
+    encrypted_password: {
         type: String,
         required: true
     },
-    salt:String,
-    role:{
-        type:Number,
-        default:0
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
     },
     purchase: {
         type: Array,
-        default:[]
+        default: []
     }
-}, {timestamps: true})
+}, {
+    timestamps: true
+})
 
+// virtual field
 user_schema.virtual("password")
-    .set(function(password){
+    .set(function (password) {
         this._password = password
         this.salt = uuidv1()
         this.encrypted_password = this.getSecurePassword(password)
+
     })
-    .get(function(){
+    .get(function () {
         return this._password
     })
 
-user_schema.method = {
+user_schema.methods = {
 
-    getSecurePassword: function(userpassword){
-        if(!password) return ""
+    getSecurePassword: function (userpassword) {
+        if (!userpassword) return ""
         try {
-            return crypto.createHmac('sha256', this,salt)
-            .update(userpassword)
-            .digest('hex')
+            return crypto.createHmac('sha256', this.salt)
+                .update(userpassword)
+                .digest('hex')
+
         } catch (error) {
             return ""
         }
     },
-    authenticate: function(userpassword){
+    authenticate: function (userpassword) {
         return this.getSecurePassword(userpassword) === this.encrypted_password
     }
 }
 
-module.exports = mongoose.model("user",user_schema)
-
+module.exports = mongoose.model("User", user_schema)
